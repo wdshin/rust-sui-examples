03543d048c872c0d08e81eb1be31511ec0bbbc2c
ElliotShin <wodshin@gmail.com>
2022-11-25 22:07:04 +0900

    trying sui client

---
+++ gui-mint-nft/src/main.rs
@@ -0,0 +1,25 @@
+use dioxus::prelude::*;
+// use std::str::FromStr;
+// use sui_sdk::types::base_types::SuiAddress;
+// use sui_sdk::SuiClient;
+
+fn main() {
+    //let sui_future = sui_main();
+    //block_on(future);
+    dioxus::desktop::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx! (
+        div { "Hello, world!" }
+    ))
+}
+
+// //#[tokio::main]
+// async fn sui_main() -> Result<(), anyhow::Error> {
+//     let sui = SuiClient::new("https://fullnode.devnet.sui.io:443", None, None).await?;
+//     let address = SuiAddress::from_str("0xec11cad080d0496a53bafcea629fcbcfff2a9866")?;
+//     let objects = sui.read_api().get_objects_owned_by_address(address).await?;
+//     println!("{:?}", objects);
+//     Ok(())
+// }
