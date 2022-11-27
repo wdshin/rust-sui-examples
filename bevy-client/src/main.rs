9282340a08cab0de32c9e009cf6a0e85c917cfe7
ElliotShin <wodshin@gmail.com>
2022-11-27 18:11:57 +0900

    bevy client added

---
+++ bevy-client/src/main.rs
@@ -0,0 +1,44 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(
+    time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
--- dmud_poc/README.md
+++ dmud_poc/README.md
@@ -1,4 +1,15 @@
 
+# dmud - 'Proof of Concepts'
+
+1. interact with sui move contracts
+2. minting objects
+3. listening events 
+4. very simple game objects
+5. very simple user object
+6. very simple player object
+7. very simple game world
+8. very simple social world
+9. entity component system in sui move
 
 ## build
 
