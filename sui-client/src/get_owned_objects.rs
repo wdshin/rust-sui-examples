b483c92381ef295b1d17a74e5c30f3f86abdcd85
ElliotShin <wodshin@gmail.com>
2022-11-28 22:54:10 +0900

    doing move_call in rust

--- sui-client/src/get_owned_objects.rs
+++ sui-client/src/get_owned_objects.rs
@@ -1,6 +1,7 @@
 use std::str::FromStr;
 use sui_sdk::types::base_types::SuiAddress;
 use sui_sdk::SuiClient;
+use sui_keys::keystore::{AccountKeystore, FileBasedKeystore, Keystore};
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -8,8 +9,28 @@ async fn main() -> Result<(), anyhow::Error> {
     let sui = SuiClient::new("https://fullnode.devnet.sui.io:443", None, None).await?;
     // todo default address 는 sui client active-address 로  가져오고
     // todo 또는 argument 로 받아서 사용하도록 변경
-    let address = SuiAddress::from_str("0xfe78ab1acc4b10abe464aeb7eaafed6125a79c42")?;
-    let objects = sui.read_api().get_objects_owned_by_address(address).await?;
-    println!("{:?}", objects);
+
+    let keystore_path = match dirs::home_dir() {
+        Some(v) => v.join(".sui").join("sui_config").join("sui.keystore"),
+        None => panic!("Cannot obtain home directory path"),
+    };
+
+    let keystore = Keystore::from(FileBasedKeystore::new(&keystore_path)?);
+    let addresses = keystore.addresses();
+    for val  in addresses.iter() {
+        println!("Addr: {}", val.to_string());
+
+        let address = SuiAddress::from_str(val.to_string().as_str())?;
+        let objects = sui.read_api().get_objects_owned_by_address(address).await?;
+
+        let mut i = 0;
+        for val  in objects.iter() {
+            println!("Object: {} {} {} {}", i, val.object_id , val.version, val.type_);
+            i+=1;
+        }
+    }
+
+    // todo select from the addresses
+    //println!("{:?}", objects);
     Ok(())
 }
--- sui-client/src/mint_nft.rs
+++ sui-client/src/mint_nft.rs
@@ -20,6 +20,61 @@ async fn main() -> Result<(), anyhow::Error> {
         None => panic!("Cannot obtain home directory path"),
     };
 
+    let address = SuiAddress::from_str("0xfe78ab1acc4b10abe464aeb7eaafed6125a79c42")?;
+    
+    // Create a move call transaction using the TransactionBuilder API.
+    let create_call = sui
+        .transaction_builder()
+        .move_call(
+            address,
+            ObjectID::from_str("0xab17cf439261ca249d27f54abc7728e949278100")?,//self.game_package_id,
+            "dmud_poc",
+            "devnet_nft",
+            vec![], //Vec<SuiTypeTag>
+            vec![], //Vec<SuiJsonValue>,
+            None, // The gateway server will pick a gas object belong to the signer if not provided.
+            1000,
+        )
+        .await?;
 
     Ok(())
 }
+
+/* 
+SuiClientCommands::CreateExampleNFT {
+    name,
+    description,
+    url,
+    gas,
+    gas_budget,
+} => {
+    let args_json = json!([
+        unwrap_or(&name, EXAMPLE_NFT_NAME),
+        unwrap_or(&description, EXAMPLE_NFT_DESCRIPTION),
+        unwrap_or(&url, EXAMPLE_NFT_URL)
+    ]);
+    let mut args = vec![];
+    for a in args_json.as_array().unwrap() {
+        args.push(SuiJsonValue::new(a.clone()).unwrap());
+    }
+    let (_, effects) = call_move(
+        ObjectID::from(SUI_FRAMEWORK_ADDRESS),
+        "devnet_nft",
+        "mint",
+        vec![],
+        gas,
+        gas_budget.unwrap_or(100_000),
+        args,
+        context,
+    )
+    .await?;
+    let nft_id = effects
+        .created
+        .first()
+        .ok_or_else(|| anyhow!("Failed to create NFT"))?
+        .reference
+        .object_id;
+    let object_read = context.client.read_api().get_parsed_object(nft_id).await?;
+    SuiClientCommandResult::CreateExampleNFT(object_read)
+}
+*/
